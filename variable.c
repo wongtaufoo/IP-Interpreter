f5e87dbbb3fd123d4b3631c6be11d6433de3326c
romanpavloff2006-sudo <romanpavloff2006@gmail.com>
2026-01-28 02:20:02 +0100

    Add files via upload

--- variable.c
+++ variable.c
@@ -1,12 +1,22 @@
-#include "variables.h"
+#include <stdlib.h>
+#include "memory.c"
+
+
+typedef struct Variable {
+    char name;
+    int start;
+    int size;
+    struct Variable *next;
+} Variable;
 
 static Variable *head = NULL;
 
-void variables_init() {
+void variables_init(void) {
     head = NULL;
 }
 
-void variables_free() {
+
+void variables_free(void) {
     Variable *cur = head;
     while (cur) {
         Variable *next = cur->next;
@@ -19,14 +29,20 @@ void variables_free() {
 Variable* var_get(char name) {
     Variable *cur = head;
     while (cur) {
-        if (cur->name == name) return cur;
+        if (cur->name == name)
+            return cur;
         cur = cur->next;
     }
     return NULL;
 }
 
+
 void var_set(char name, int start, int size) {
     Variable *v = malloc(sizeof(Variable));
+    if (!v) {
+         exit(0);
+    }
+
     v->name = name;
     v->start = start;
     v->size = size;
@@ -34,17 +50,23 @@ void var_set(char name, int start, int size) {
     head = v;
 }
 
+
 void var_delete(char name) {
     Variable *cur = head;
     Variable *prev = NULL;
 
     while (cur) {
         if (cur->name == name) {
-            if (prev) prev->next = cur->next;
-            else head = cur->next;
+            if (prev)
+                prev->next = cur->next;
+            else
+                head = cur->next;
+
             free(cur);
             return;
         }
         prev = cur;
         cur = cur->next;
-     
+    }
+}
+
