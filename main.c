c4e7cfee9ebc8a0788d54d9ba61fc961364c2fad
CamelliaZiyuZhou <camellia.susu@gmail.com>
2026-01-28 17:42:03 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,3 +5,24 @@
 #include "fetch.h"
 #include "structField.h"
 #include “commands.c"
+
+#include "project.h"
+
+int main(){
+    //initial Memory heap + the empty Variable list
+    init_memory();      // From memory.c
+    variables_init();   // From variable.c 
+
+    while (1) {
+        struct fetched_command current_fc = fetching(argv[1]);
+        // when reach the end of the input
+        if (current_fc.OpName == NULL){
+          break;
+        }
+        decode(current_fc);
+    }
+
+    // Free the linked list and the heap memory before exiting
+    variables_free(); 
+    return 0;
+}
