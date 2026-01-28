da6c29e10a6a2c85d1735fc1ea943b38a178f0f7
wongtaufoo <bonglkv2@gmail.com>
2026-01-28 19:11:15 +0100

    Update memory.c

--- memory.c
+++ memory.c
@@ -4,52 +4,106 @@
 
 #define TotalMemory 100
 
+
 typedef struct Mem{
     int *m;// pointer to heaps
+    int avail_cells;
+    EmptyBlock *head;
     } Mem;
 
-typedef struct Var{
+typedef struct AssignedBlock{ //filled block
     int start;
     int end;
-    Var* ptr;
-    } Var;
+    } AssignedBlock;
 
-int avail;
-int avail_start;
+typedef struct EmptyBlock{
+    int start;
+    int end;
+    int size=end-start;
+    struct EmptyBlock *next; //pointer to next freeblock
+}
+
+Mem * Memory;
 
 void init_memory{
-    Mem * Memory = malloc(sizeof(Mem));
+
+    Memory = malloc(sizeof(Mem));
+
+    if (Memory==NULL){ // allocation fail
+        fprintf(stderr, "Not enough memory.\n");
+        exit(0);
+    }
+
     Memory->m = malloc(TotalMemory*sizeof(int));
 
-    if (Memory->m==NULL){ //unallocated
+    if (Memory->m==NULL){ //point to nothing i.e. allocation fail
         fprintf(stderr, "Not enough memory.\n");
         free(Memory);
         exit(0);
-    avail=TotalMemory;
-    avail_start=0;
     }
-}
 
-Var * Mal(char c,int n){
-    if (n>avail){
-        fprintf(stderr,"Not enough memory.\n");
+    Memory->head= malloc(sizeof(EmptyBlock));
+
+    if (Memory->head==NULL){ //fail allocation
+        fprintf(stderr, "Not enough memory.\n");
+        free(Memory->m);
+        free(Memory);
         exit(0);
     }
-    Var *x = malloc(sizeof(Var));
-    //store new start and n in struct
-    x->name=c;
-    x->start=avail_start;
-    x->end=avail_start+n;
 
-    for (int i=x->start; i<x->end; i++){
-        NewMemory->m[i]=0;
+    Memory->avail_cells=TotalMemory;
+    Memory->head->start=0;
+    Memory->head->end=TotalMemory;
+    Memory->head->next=NULL;
     }
 
-    avail_start+=n;
-    avail-=n;
-    
-    return x;
+AssignedBlock * allocate(int n){
+    if (n>avail){
+        return NULL;
+    }
+    EmptyBlock **prev= &Memory->head;
+    EmptyBlock *current= Memory->head;
+
+    while (current!=NULL){
+        int size= current->end-current->start;
+        if (n<=size){
+            AssignedBlock *New = malloc(sizeof(AssignedBlock));
+            New->start= current->start;
+            New->end=New->start+n;
+
+            //if empty block larger than needed, block is split and start of empty block updated
+            if (n<size)){
+                current->start+=n;
+            }
+            else{ // perfect fit, current cell is now assigned
+                *prev=current->next;
+                free(current);
+            }
+            Memory->avail_cells-=n;
+            return New;
+        }
+        // check next empty block until a block with a size large enough is found
+        prev = &current->next;
+        current=current->next;
+    }
+    return NULL; //no empty block found
 }
 
+void free_block(AssignedBlock *b){
+    if (b==NULL)return;
 
+    EmptyBlock *newFree=malloc(sizeof(EmptyBlock));
+    newFree->start=b->start;
+    newFree->end=b->end;
+
+    newFree->next=Memory->head; //add to beginning of empty block list
+    Memory->head=newFree;
+    int size= b->start-b->end
+    Memory->avail_cells+=size;
+
+    free(b);
+}
+
+int read(int position){
+    if (position>=TotalMemory)}
 
