0f65153fb50be8b3f94ad3d680eac0205f8fc961
CamelliaZiyuZhou <camellia.susu@gmail.com>
2026-01-28 16:19:33 +0100

    Update commands.c

--- commands.c
+++ commands.c
@@ -1,81 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-// Mal x n
-void Mal(Var *v, char name, int n, int start) {
-    v->name = name;
-    v->start = start;
-    v->end = start + n - 1;
-    
-    for (int i = v->start; i <= v->end; i++) {
-        Memory[i] = 0;
+//decoding the input stored in struct fetched_command fc
+void decode(struct fetched_command fc) {
+    if (strcmp(fc.OpName, "Mal") == 0) {
+        // Call Mal in memory.c to get space 
+        Var *new_mem = Mal(fc.par1, fc.ipar2);
+        // Call var_set from variable.c to register the name and range
+        var_set(fc.par1, new_mem->start, new_mem->end);
+        free(new_mem); 
+        return;
     }
-}
-//allocate(5)
-//creat(name,Meory) in variables
 
-// Ass x n
-void Ass(Var *v, int n) {
-    Memory[v->start] = n;
-}
+    // Get the primary variable 'x' from the variable tracker
+    Variable *x = var_get(fc.par1);
+    if (x == NULL) return; 
 
-
-// Inc x n
-void Inc(Var *v, int n) {
-    Memory[v->start + n]++;
-}
-
-// Dec x n
-void Dec(Var *v, int n) {
-    Memory[v->start + n]--;
-}
-
-// Pri x n
-void Pri(Var *v, int n) {
-    printf("%d\n", Memory[v->start + n]);
-}
-
-void Add(Var *x, Var *y) {
-    Memory[x->start] = Memory[x->start] + Memory[y->start];
-}
-
-void Sub(Var *x, Var *y) {
-    Memory[x->start] = Memory[x->start] - Memory[y->start];
-}
-
-void Mul(Var *x, Var *y) {
-    Memory[x->start] = Memory[x->start] * Memory[y->start];
-}
-
-
-// x[i] = (x[i] * y[i]) 
-void And(Var *x, Var *y) {
-    int len = x->end - x->start + 1;
-    for (int i = 0; i <= len; i++) {
-        Memory[x->start + i] = (Memory[x->start + i] * Memory[y->start + i]) % 2;
+    // Ass x n
+    if (strcmp(fc.OpName, "Ass") == 0) {
+        Memory[x->start] = fc.ipar2;
     }
-}
-
-// x[i] = (x[i] + y[i]) 
-void Xor(Var *x, Var *y) {
-    int len = x->end - x->start + 1;
-    for (int i = 0; i <= len; i++) {
-        Memory[x->start + i] = (Memory[x->start + i] + Memory[y->start + i]) % 2;
+    // Inc x n
+    else if (strcmp(fc.OpName, "Inc") == 0) {
+        Memory[x->start + fc.ipar2]++;
     }
-}
-
-// Fre x
-void Fre(Var *v) {
-    
-    v->start = -1;
-    v->end = -1;
-}
-
-// Pra x
-void Pra(Var *v) {
-    printf("[");
-    for (int i = v->start; i <= v->end; i++) {
-        printf("%d", Memory[i]);
+    // Dec x n
+    else if (strcmp(fc.OpName, "Dec") == 0) {
+        Memory[x->start + fc.ipar2]--;
     }
-    printf("]\n");
+    // Pri x n
+    else if (strcmp(fc.OpName, "Pri") == 0) {
+        printf("%d\n", Memory[x->start + fc.ipar2]);
+    }
+    // Add x y
+    else if (strcmp(fc.OpName, "Add") == 0) {
+        Variable *y = var_get(fc.cpar2);
+        if (y) Memory[x->start] = Memory[x->start] + Memory[y->start];
+    }
+    // Sub x y
+    else if (strcmp(fc.OpName, "Sub") == 0) {
+        Variable *y = var_get(fc.cpar2);
+        if (y) Memory[x->start] = Memory[x->start] - Memory[y->start];
+    }
+    // Mul x y
+    else if (strcmp(fc.OpName, "Mul") == 0) {
+        Variable *y = var_get(fc.cpar2);
+        if (y) Memory[x->start] = Memory[x->start] * Memory[y->start];
+    }
+    // And x y (x[i] = (x[i] * y[i]) %2)
+    else if (strcmp(fc.OpName, "And") == 0) {
+        Variable *y = var_get(fc.cpar2);
+        if (y) {
+            int len = x->end - x->start; 
+            for (int i = 0; i < len; i++) {
+                Memory[x->start + i] = (Memory[x->start + i] * Memory[y->start + i]) % 2;
+            }
+        }
+    }
+    // Xor x y (x[i] = (x[i] + y[i]) %2)
+    else if (strcmp(fc.OpName, "Xor") == 0) {
+        Variable *y = var_get(fc.cpar2);
+        if (y) {
+            int len = x->end - x->start;
+            for (int i = 0; i < len; i++) {
+                Memory[x->start + i] = (Memory[x->start + i] + Memory[y->start + i]) % 2;
+            }
+        }
+    }
+    // Fre x
+    else if (strcmp(fc.OpName, "Fre") == 0) {
+        var_delete(fc.par1); // delete from variable tracking
+    }
+    // Pra x
+    else if (strcmp(fc.OpName, "Pra") == 0) {
+        printf("[ ");
+        for (int i = x->start; i < x->end; i++) {
+            printf("%d ", Memory[i]);
+        }
+        printf("]\n");
+    }
 }
