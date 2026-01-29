dbeff426ca0bb7441ec227e9dbbf9b0073423d78
wongtaufoo <bonglkv2@gmail.com>
2026-01-29 04:19:59 +0100

    Update memory.h

--- memory.h
+++ memory.h
@@ -1,31 +1,44 @@
-#ifndef MEMORY_H
-#define MEMORY_H
+#ifndef MEMORY
+#define MEMORY
 
 #define TOTAL_MEMORY 100
 
+//list of empty blocks
 typedef struct EmptyBlock{
     int start;
     int end;
     struct EmptyBlock *next; //pointer to next freeblock
 } EmptyBlock;
 
+// memory created
 typedef struct Mem{
-    int *m;// pointer to heaps
+    int *m;// pointer to heap
     int avail_cells;
     EmptyBlock *head;
     } Mem;
 
-typedef struct AssignedBlock{ //filled block
+//list of filled blocks
+typedef struct AssignedBlock{
     int start;
     int end;
     } AssignedBlock;
 
+// use once in main to create memory
 void init_memory(void);
+
+// get empty memory block that can be allocated
 AssignedBlock* allocate(int n);
+
+// free assigned block and mark it as available for use
 void free_block(AssignedBlock* b);
+
+// get int stored in memory cell
 int read_cell(int pos);
+
+// write int into memory cell
 void write_cell(int pos, int val);
 
+// make memory global for use in other files
 extern Mem* Memory;
 
 #endif
