e40591f85a0e050d9224c9f41a267928a7479051
zooietje <zooietje@gmail.com>
2026-01-29 15:36:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 #include "commands.h"
 #include "variable.h"
 
-int main(void){
+int main(cons char* fileName){
     //initial Memory heap + the empty Variable list
     init_memory();      // From memory.c
     variables_init();   // From variable.c
 
-    const char* filename = "test.txt";
-    int n = amount_commands("test.txt");
+    const char* filename = fileName;
+    int n = amount_commands(filename);
     for (int i = 0; i <= n-1; i++) {                 // start at 0
         struct fetched_command fc = fetching(filename, i);
         decode(fc);
