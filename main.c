9dc3fa8d0059ff2fce022dea46395c5ae9e754e9
wongtaufoo <bonglkv2@gmail.com>
2026-01-29 15:46:12 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,15 +6,14 @@
 #include "commands.h"
 #include "variable.h"
 
-int main(cons char* fileName){
+int main(const char* fileName){
     //initial Memory heap + the empty Variable list
     init_memory();      // From memory.c
     variables_init();   // From variable.c
 
-    const char* filename = fileName;
-    int n = amount_commands(filename);
+    int n = amount_commands(fileName);
     for (int i = 0; i <= n-1; i++) {                 // start at 0
-        struct fetched_command fc = fetching(filename, i);
+        struct fetched_command fc = fetching(fileName, i);
         decode(fc);
     }
     // Free the linked list and the heap memory before exiting
